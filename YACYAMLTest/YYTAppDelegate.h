4945b5a65b44456466ef105b1a20ecca82bd8b59
James Montgomerie <jamie@th.ingsmadeoutofotherthin.gs>
2013-01-07 21:06:35 +0000

    Project as created by Xcode, no changes yet.

---
+++ YACYAMLTest/YYTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YYTAppDelegate.h
+//  YACYAMLTest
+//
+//  Created by James Montgomerie on 07/01/2013.
+//  Copyright (c) 2013 James Montgomerie. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface YYTAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ YACYAMLTest/YYTAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  YYTAppDelegate.m
+//  YACYAMLTest
+//
+//  Created by James Montgomerie on 07/01/2013.
+//  Copyright (c) 2013 James Montgomerie. All rights reserved.
+//
+
+#import "YYTAppDelegate.h"
+
+@implementation YYTAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
